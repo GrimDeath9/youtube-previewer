33cc9cd044a27ce49d1cb3d9aa3245dcf28f05b4
Grim <gdeath909@gmail.com>
2024-06-28 04:16:20 -0600

    Improved variaables, made into ContextManager

--- youtube-previewer/archive.py
+++ youtube-previewer/archive.py
@@ -1,28 +1,30 @@
-class Archiver:
+class Archive:
     """
-    Class used to simplify interacting with archive.
+    Class used to simplify interacting with archive of already seen links.
     """
     def __init__(self, filepath: str):
         self.file = filepath
-        self.archive = self.__readFile()
-        self.to_add = []
+        self.archive = self.__read_archive()
+        self.buffer = []
+    
+    def __enter__(self):
+        return self
 
-    def __readFile(self):
+    def __read_archive(self) -> list[str]:
         with open(self.file, 'r') as f:
             archive = f.readlines()
             f.close()
-        return [i.split("\n")[0] for i in archive]
-    
-    def get_archive(self):
-        return self.archive
+        return [i.replace('\n', '') for i in archive]
 
     def add(self, id):
-        if id not in self.archive:
-            self.toAdd.append(id)
+        self.buffer.append(id)
     
-    def save(self, file = 'archive.txt'):
-        to_add = [i for i in self.to_add if i not in self.archive]
-        with open(file, 'a') as f:
+    def save(self):
+        to_add = [i for i in self.buffer if i not in self.archive]
+        with open(self.file, 'a') as f:
             for i in to_add:
                 f.write(f'{i}\n')
             f.close()
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.save()
