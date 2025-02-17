35f82ada313d65fed066efea215c7cf76901d08c
Grim <gdeath909@gmail.com>
2025-02-16 20:09:56 -0700

    Renamed archive to database, added section for keybinds

--- youtube-previewer/config.py
+++ youtube-previewer/config.py
@@ -4,12 +4,13 @@ class Config:
     def __init__(self, filepath):
         data = tomllib.load(open(filepath, 'rb'))
         self.input = data['read']['read']
-        self.archive = data['read']['archive']
+        self.database = data['read']['archive']
         self.check = data['output']['check']
         self.removed = data['output']['check']
         self.optimize = data['display']['optimize_layout']
         self.by_month = data['display']['group_by_month']
         self.misc = data['output']['misc']
+        self.keybind = data['keybinds']
 
     def __str__(self) -> str:
         return str(self.__dict__)
